bf3e94a9b2a4f3000fe1267c89346e66380c13ff
chenl <244776516@qq.com>
2022-05-26 14:40:31 +0800

    修复mybatis写错的SQL。

---
+++ src/test/java/com/example/sqlexamine/DataStructTest.java
@@ -0,0 +1,18 @@
+package com.example.sqlexamine;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Author chenl
+ * @Date 2022/5/26 9:52 上午
+ */
+
+public class DataStructTest {
+
+
+    @Test
+    //二叉树
+    public void twoTree(){
+
+    }
+}
