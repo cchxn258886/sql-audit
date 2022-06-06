f4d04a0fa0c058376eb59520c99da6f9f5a5a1e5
chenl <244776516@qq.com>
2022-06-06 16:58:40 +0800

    删除密钥

--- src/main/java/com/example/sqlexamine/service/rdsservice/context/RdsConfigContext.java
+++ src/main/java/com/example/sqlexamine/service/rdsservice/context/RdsConfigContext.java
@@ -5,11 +5,6 @@ import lombok.Data;
 /**
  * @Author chenl
  * @Date 2022/4/18 5:33 下午
- *     private static String AccessKeyID = "LTAI5tHDVAoessXQ5GVNjXZg";
- *     private static String AccessKeySecret = "UMTPwvJNHG51NRpRiddKSVdNwTf5aD";
- *     private static String regionId = "cn-hangzhou";
- *     private static String endpoint = "rm-bp1b476e1uxk2uo8goo.mysql.rds.aliyuncs.com";//测试数据库
- *     private static String endPoint2 = "rds.aliyuncs.com";
  */
 @Data
 public class RdsConfigContext {
